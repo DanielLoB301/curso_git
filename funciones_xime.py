dae0056b9399c569456df7631a1fb3058d85b1e6
Ximena <cindy.mendoza@axity.com>
2026-05-05 16:59:48 -0600

    Revert "agregar variables"
    This reverts commit 6d95e2f724b399f29272c85345047e1309ca6b40.

--- funciones_xime.py
+++ funciones_xime.py
@@ -1,4 +1,7 @@
-def suma(a, b)
+def suma(a, b):
+
+
+
     return a + b
 def resta(a, b):
     return a - b
@@ -8,8 +11,3 @@ def division(a, b):
     if b == 0:
         return "Error: no se puede dividir entre 0"
     return a / b
-print(suma(5, 3))            # 8
-print(resta(5, 3))           # 2
-print(multiplicacion(5, 3))  # 15
-print(division(6, 3))        # 2.0
-
