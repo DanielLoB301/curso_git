c1ff0a00c2395b08b12cc803d43efced998b5214
Miriam Lizbeth Ramírez Torres <miriam.ramirez@axity.com>
2026-05-05 17:28:18 -0600

    Correción codigo

--- funciones_miriam.py
+++ funciones_miriam.py
@@ -3,13 +3,10 @@ def sumar (a,b):
 
 def restar (a,b):
     return a - b 
-<<<<<<< HEAD
 
 def multiplicacion (a,b):
     return a * b
 
 def division (a, b):
     return a/b
-    
-=======
->>>>>>> parent of 41f1239 (Agregar funcion multiplicacion)
+ 
