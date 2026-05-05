6375930587e9c21709008140521569380fa3051e
Victor Martinez <vicmanuelg19@gmail.com>
2026-05-05 16:47:03 -0600

    Se arregla la función de multiplicacion

--- funsiones_victorMartinez.py
+++ funsiones_victorMartinez.py
@@ -5,3 +5,7 @@ def suma(num_a, num_b):
 #Función resta de dos numeros
 def resta(num_a, num_b):
     return num_a - num_b
+
+#Función multiplicación de dos numeros
+def multiplicacion(num_a, num_b):
+    return num_a * num_b
