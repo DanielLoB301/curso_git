4a92ee5ef167d774173074a170a02a11fa6b2f68
Dani1dh <ddaniel.hernandeza@axity.com>
2026-05-05 15:14:52 -0600

    Modificacion desde rama saliente de develop

--- usuarios_daniel.py
+++ usuarios_daniel.py
@@ -1,3 +1,3 @@
 def create_user(username, enamil):
 
-    print(f"Usuario '{username}' con correo '{email}' ha sido creado.")
+    print(f"Usuario '{username}' con correo '{email}' ha sido creado con la funcion de daniel.")
