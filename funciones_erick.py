b41e1ef315352365daf4985eb4d0e77b54630d76
Erick Sebastian Meneses Navarro <erick.meneses@axity.com>
2026-05-05 16:38:07 -0600

    feature/Se agrega funcion dividir

--- funciones_erick.py
+++ funciones_erick.py
@@ -6,3 +6,9 @@ def restar(a, b):
 
 def multiplicar(a, b):
     return a * b
+
+def dividir(a, b):
+    if b == 0:
+        print("Error: no se puede dividir entre cero")
+        return -1
+    return a / b
