1d18a93083cc9220665c02c82740e5f1e09c1dfd
Ricardo Ramos Venegas <ricardo.ramos@axity.com>
2026-05-05 17:22:02 -0600

    Cambio funcion division

--- funciones_ricardo.py
+++ funciones_ricardo.py
@@ -12,7 +12,7 @@ def Multiplicacion(num1, num2):
 
 #Función división
 def Division(num1, num2):
-    return num1 / num2
+    return num1 ** num2
 
 #Función raiz cuadrada
 def RaizCuadrada(num1, num2):
