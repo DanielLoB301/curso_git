8360313c70e111aa191b421de3d75d2a496f2611
Erick Sebastian Meneses Navarro <erick.meneses@axity.com>
2026-05-05 17:01:50 -0600

    hotfix/Se cambia mensaje de error en funcion dividir

--- funciones_erick.py
+++ funciones_erick.py
@@ -6,9 +6,8 @@ def restar(a, b):
 
 def multiplicar(a, b):
     return a * b
-
 def dividir(a, b):
     if b == 0:
-        print("Error: no se puede dividir entre cero")
+        print("Imposible dividir entre cero")
         return -1
     return a / b
