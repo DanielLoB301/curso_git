312a5357d5cf9a5d89e680fed1edcc55f8a31424
Victor Martinez <vicmanuelg19@gmail.com>
2026-05-05 16:48:16 -0600

    Se agrega función de división

--- funsiones_victorMartinez.py
+++ funsiones_victorMartinez.py
@@ -9,3 +9,7 @@ def resta(num_a, num_b):
 #Función multiplicación de dos numeros
 def multiplicacion(num_a, num_b):
     return num_a * num_b
+
+#Función división de dos numeros
+def division(num_a, num_b):
+    return num_a / num_b
