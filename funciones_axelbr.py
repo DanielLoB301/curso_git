aaefaee7665dd7650eb655329302d830940cb7c6
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2026-05-05 16:42:12 -0600

    Se agrega funcion numero primo y se corrige el factorial

--- funciones_axelbr.py
+++ funciones_axelbr.py
@@ -1,9 +1,6 @@
-def sumar(a, b):
-    return a + b
-def restar(a, b):
-    return a - b
-def multiplicar(a, b):
-    return a * b
+def sumar(a, b):return a + b
+def restar(a, b):return a - b
+def multiplicar(a, b):return a * b
 def dividir(a, b):
     if b != 0:
         return a / b
@@ -25,3 +22,11 @@ def factorial(n):
         resultado = 1
         for i in range(2, n + 1):
             resultado *= i
+        return resultado
+def es_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
