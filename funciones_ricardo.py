3711c1bf3507a0c78efd2a843e3cebf530420ff0
Ricardo Ramos Venegas <ricardo.ramos@axity.com>
2026-05-05 16:36:14 -0600

    Nueva funcion raiz cuadrada

--- funciones_ricardo.py
+++ funciones_ricardo.py
@@ -13,3 +13,7 @@ def Multiplicacion(num1, num2):
 #Función división
 def Division(num1, num2):
     return num1 / num2
+
+#Función raiz cuadrada
+def RaizCuadrada(num1, num2):
+    return pow(num1,num2)
