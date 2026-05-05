3154874c330896fdc8e022cb0a0121ec91dc6d94
Oscar Uriel López Barradas <oscar.lopezb@axity.com>
2026-05-05 16:36:57 -0600

    Se añade la función resta

--- funciones_oscar.py
+++ funciones_oscar.py
@@ -1,4 +1,7 @@
 def suma2(num_1,num_2):
     m= num_1+num_2
     print(f"La suma de {num_1} con {num_2} es {m}")
-        
+
+def resta2(num1, num2):
+    n = num2-num1
+    print(f"La resta de {num1} de {num2} es {n}")
