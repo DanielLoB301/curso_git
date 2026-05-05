7b02f5f4ebdbb2d83872c1ff7002b7a79b733c6c
Oscar Uriel López Barradas <oscar.lopezb@axity.com>
2026-05-05 16:42:45 -0600

    Se revierte

--- funciones_oscar.py
+++ funciones_oscar.py
@@ -1,6 +1,7 @@
 def suma2(num_1,num_2):
     m= num_1+num_2
     print(f"La suma de {num_1} con {num_2} es {m}")
+<<<<<<< HEAD
 
 def resta2(num1, num2):
     n = num2-num1
@@ -9,3 +10,6 @@ def resta2(num1, num2):
 def division(no1,no2):
     p = no1/no2
     print(f"El cociente entre {no1} sobre {no2} es {p}")
+=======
+        
+>>>>>>> parent of 3154874 (Se añade la función resta)
