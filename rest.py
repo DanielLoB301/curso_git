78f5c91fe91a3b71f34c16d213f0b7bf097cd86e
Daniel Belmonte <daniel.lopezb@axity.com>
2026-05-05 15:36:27 -0600

    test restore

---
+++ rest.py
@@ -0,0 +1,1 @@
+print("Commmit 1 ")
---
+++ usuarios.py
@@ -0,0 +1,4 @@
+def create_user(username, email):
+    # Lógica para crear un nuevo usuario
+    print(f"Usuario con username correo '{email}' ha sido creado.")
+
